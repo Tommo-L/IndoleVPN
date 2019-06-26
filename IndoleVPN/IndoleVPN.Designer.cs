ba0d8968e4fa47285d9566a5868c9ef8988dea72
luchuan <luchuan@neo.org>
2019-06-26 10:27:43 +0800

    up code

---
+++ IndoleVPN/IndoleVPN.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace IndoleVPN
+{
+    partial class IndoleVPN
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IndoleVPN));
+            this.label1 = new System.Windows.Forms.Label();
+            this.remoteProxyTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.aesKeyTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.localProxyPortTxt = new System.Windows.Forms.TextBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.OpenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ConfigToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CloseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "远程代理";
+            // 
+            // remoteProxyTxt
+            // 
+            this.remoteProxyTxt.Location = new System.Drawing.Point(116, 33);
+            this.remoteProxyTxt.Name = "remoteProxyTxt";
+            this.remoteProxyTxt.Size = new System.Drawing.Size(213, 21);
+            this.remoteProxyTxt.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "AES加密Key";
+            // 
+            // aesKeyTxt
+            // 
+            this.aesKeyTxt.Location = new System.Drawing.Point(116, 83);
+            this.aesKeyTxt.Name = "aesKeyTxt";
+            this.aesKeyTxt.Size = new System.Drawing.Size(213, 21);
+            this.aesKeyTxt.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "本地代理";
+            // 
+            // localProxyPortTxt
+            // 
+            this.localProxyPortTxt.Location = new System.Drawing.Point(116, 133);
+            this.localProxyPortTxt.Name = "localProxyPortTxt";
+            this.localProxyPortTxt.Size = new System.Drawing.Size(213, 21);
+            this.localProxyPortTxt.TabIndex = 5;
+            this.localProxyPortTxt.Text = "localhost:8091";
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(254, 183);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 6;
+            this.saveBtn.Text = "保存";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "ElephantVPN";
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenToolStripMenuItem,
+            this.ConfigToolStripMenuItem,
+            this.CloseToolStripMenuItem,
+            this.ExitToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(125, 92);
+            // 
+            // OpenToolStripMenuItem
+            // 
+            this.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem";
+            this.OpenToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.OpenToolStripMenuItem.Text = "开启代理";
+            this.OpenToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // ConfigToolStripMenuItem
+            // 
+            this.ConfigToolStripMenuItem.Name = "ConfigToolStripMenuItem";
+            this.ConfigToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.ConfigToolStripMenuItem.Text = "配置代理";
+            this.ConfigToolStripMenuItem.Click += new System.EventHandler(this.ConfigToolStripMenuItem_Click);
+            // 
+            // CloseToolStripMenuItem
+            // 
+            this.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem";
+            this.CloseToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.CloseToolStripMenuItem.Text = "关闭代理";
+            this.CloseToolStripMenuItem.Click += new System.EventHandler(this.CloseToolStripMenuItem_Click);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.ExitToolStripMenuItem.Text = "退出";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(23, 182);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 7;
+            this.startBtn.Text = "运行";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(139, 182);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(75, 23);
+            this.stopBtn.TabIndex = 8;
+            this.stopBtn.Text = "停止";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.CloseToolStripMenuItem_Click);
+            // 
+            // IndoleVPN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 218);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.localProxyPortTxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.aesKeyTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.remoteProxyTxt);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "IndoleVPN";
+            this.Text = "IndoleVPN";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ElephantVPN_FormClosing);
+            this.Resize += new System.EventHandler(this.ElephantVPN_Resize);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox remoteProxyTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox aesKeyTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox localProxyPortTxt;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem OpenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ConfigToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CloseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+    }
+}
+
---
+++ IndoleVPN/IndoleVPN.cs
@@ -0,0 +1,245 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace IndoleVPN
+{
+    public partial class IndoleVPN : Form
+    {
+        [DllImport("wininet.dll")]
+        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
+        public const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
+        public const int INTERNET_OPTION_REFRESH = 37;
+
+        Process indoleExe;
+
+        public IndoleVPN()
+        {
+            InitializeComponent();
+            loadConfig();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void loadConfig()
+        {
+            XElement configXml = XElement.Load(@"config.xml");
+            if (configXml == null) return;
+
+            XElement tcpaes = configXml.Element("tcpaes");
+            if (tcpaes == null) return;
+
+            localProxyPortTxt.Text = tcpaes.Attribute("address").Value.ToString();
+
+            XElement encode = tcpaes.Element("encode");
+            if (encode != null)
+            {
+                XElement aesenc = encode.Element("aesenc");
+                aesKeyTxt.Text = aesenc.Attribute("hex_key").Value.ToString();
+            }
+
+            XElement decode = tcpaes.Element("decode");
+            if (decode != null)
+            {
+                XElement aesdec = decode.Element("aesdec");
+                aesKeyTxt.Text = aesdec.Attribute("hex_key").Value.ToString();
+            }
+
+            XElement tcp = tcpaes.Element("tcp");
+            if (tcp == null) return;
+
+            remoteProxyTxt.Text = tcp.Attribute("address").Value.ToString();
+        }
+
+        private XElement buildXML()
+        {
+            XElement configXml = new XElement("indole",
+                                  new XElement("tcpaes",
+                                          new XAttribute("network", "tcp"),
+                                          new XAttribute("address", localProxyPortTxt.Text),
+                                          new XAttribute("bufsize", "1024"),
+
+                                          new XElement("encode",
+                                                      new XElement("aesenc",
+                                                                  new XAttribute("queue_size", "1024"),
+                                                                  new XAttribute("hex_key", aesKeyTxt.Text))),
+                                          new XElement("decode",
+                                                  new XElement("aesdec",
+                                                                  new XAttribute("queue_size", "1024"),
+                                                                  new XAttribute("hex_key", aesKeyTxt.Text),
+                                                                  new XAttribute("buf_size", "65536"))),
+                                          new XElement("tcp",
+                                                  new XAttribute("network", "tcp"),
+                                                  new XAttribute("address", remoteProxyTxt.Text))));
+            return configXml;
+        }
+
+        private void refreshConfig()
+        {
+            XElement configXml = buildXML();
+            configXml.Save(@"config.xml");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            refreshConfig();
+            MessageBox.Show("保存成功", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+
+        private void ElephantVPN_Resize(object sender, EventArgs e)
+        {
+            if(this.WindowState == FormWindowState.Minimized)
+            {
+                this.Visible = false;
+                notifyIcon1.Visible = true;
+            }
+            else
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            showNormalWindow();
+        }
+
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            exitWindow();
+        }
+
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string remoteProxy = remoteProxyTxt.Text;
+            if(remoteProxy == null || remoteProxy.Trim() == "")
+            {
+                MessageBox.Show("请配置远程代理地址和端口", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            string aesKey = aesKeyTxt.Text;
+            if (aesKey == null || aesKey.Trim() == "")
+            {
+                MessageBox.Show("请配置AES加密key", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            string localProxy = localProxyPortTxt.Text;
+            if (localProxy == null || localProxy.Trim() == "")
+            {
+                MessageBox.Show("请配置本地代理地址和端口", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+            if (indoleExe != null)
+            {
+                try
+                {
+                    indoleExe.Kill();
+                }
+                catch (Exception)
+                {
+                }
+                indoleExe.WaitForExit(3000);
+            }
+
+            indoleExe = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = @"indole.exe",
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true
+                }
+            };
+            indoleExe.Start();
+            indoleExe.StandardInput.Write(buildXML().ToString() + "\n");
+
+            RegistryKey intSettings = Registry.CurrentUser.OpenSubKey("SOFTWARE", true).OpenSubKey(@"Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+            intSettings.SetValue("ProxyEnable", 1);
+            intSettings.SetValue("ProxyServer", localProxyPortTxt.Text);
+            intSettings.Flush();
+            intSettings.Close();
+
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+
+            MessageBox.Show("启动成功", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void CloseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (indoleExe != null)
+            {
+                try
+                {
+                    indoleExe.Kill();
+                    indoleExe.WaitForExit(3000);
+                }
+                catch (Exception)
+                {
+                }
+                indoleExe = null;
+            }
+
+            RegistryKey intSettings = Registry.CurrentUser.OpenSubKey("SOFTWARE", true).OpenSubKey(@"Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+            intSettings.SetValue("ProxyEnable", 0);
+            intSettings.Flush();
+            intSettings.Close();
+
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+
+            MessageBox.Show("关闭成功", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void ConfigToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            showNormalWindow();
+        }
+
+
+        private void showNormalWindow()
+        {
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+            this.Activate();
+        }
+
+        private void exitWindow()
+        {
+            if (indoleExe != null)
+            {
+                try
+                {
+                    indoleExe.Kill();
+                    indoleExe.WaitForExit(3000);
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            RegistryKey intSettings = Registry.CurrentUser.OpenSubKey("SOFTWARE", true).OpenSubKey(@"Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+            intSettings.SetValue("ProxyEnable", 0);
+            intSettings.Flush();
+            intSettings.Close();
+
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+
+            Application.Exit();
+        }
+
+        private void ElephantVPN_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            exitWindow();
+        }
+    }
+}
